b589e36cf1b2c18b6e66cc74d9b05890f7fbee6c
Manca Strgar <manca.strgar@student.fmf.uni-lj.si>
2018-05-18 13:05:15 +0200

    urejanje

---
+++ sql.R
@@ -0,0 +1,37 @@
+# Neposredno klicanje SQL ukazov v R
+library(dplyr)
+library(dbplyr)
+library(RPostgreSQL)
+
+source("auth.R")
+
+# Povežemo se z gonilnikom za PostgreSQL
+drv <- dbDriver("PostgreSQL")
+
+# Uporabimo tryCatch,
+# da prisilimo prekinitev povezave v primeru napake
+tryCatch({
+  # Vzpostavimo povezavo
+  conn <- dbConnect(drv, dbname = db, host = host,
+                    user = user, password = password)
+  
+  # Poizvedbo zgradimo s funkcijo build_sql
+  # in izvedemo s funkcijo dbGetQuery
+  
+  dbSendQuery(conn, build_sql("CREATE TABLE vrsta (
+                               id INTEGER PRIMARY KEY,
+                               ime TEXT NOT NULL)"))
+                        
+  # Rezultat dobimo kot razpredelnico (data frame)
+}, finally = {
+  # Na koncu nujno prekinemo povezavo z bazo,
+  # saj preveč odprtih povezav ne smemo imeti
+  dbDisconnect(conn)
+  # Koda v finally bloku se izvede v vsakem primeru
+  # - bodisi ob koncu izvajanja try bloka,
+  # ali pa po tem, ko se ta konča z napako
+})
+  
+  
+  
+ 
