38286b385aa9c1e17b0f4277bd88c5883e242d83
Klemen Hovnik <klemen.hovnik@gmail.com>
2018-05-28 23:49:20 +0200

    Dodani izdelki

--- uvoz.R
+++ uvoz.R
@@ -7,11 +7,10 @@ library(digest)
 
 uvoz <- function() {
   tab <- read_csv2(file="izdelki.csv",
-                    locale = locale(encoding = "Windows-1250"), skip = 1,  n_max = 737)
+                    locale = locale(encoding = "Windows-1250"), skip = 1,  n_max = 810)
 
   colnames(tab) <- c("IZDELEK-ime","IZDELEK-id","KOLIČINA","CENA","VRSTA-ime","VRSTA-id",
                       "TRGOVINA-ime","TRGOVINA-id","TRGOVINA-naslov","PODJETJE-ime","PODJETJE-id", "PODJETJE-naslov","PODJETJE-telefon")
-  tab <- tab[,-c(14)]
   return(tab)
 }
 izdelki <- uvoz()
